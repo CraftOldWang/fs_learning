435b04b5159687f5c2c35630fc3245194257d7c3
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-20 14:53:34 +0800

    Ex1.4 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,33 +1,29 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const infos = [
-    part1,
-    part2,
-    part3
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ]
   const nums = [
-    part1.exercises,
-    part2.exercises,
-    part3.exercises
+    parts[0].exercises,
+    parts[1].exercises,
+    parts[2].exercises
   ]
 
   return (
     <div>
       <Header course = {course}/>
-      <Content infos = {infos} />
+      <Content parts = {parts} />
       <Total nums = {nums} />
     </div>
   )
@@ -45,9 +41,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <>
-      <Part info = {props.infos[0]} />
-      <Part info = {props.infos[1]} />
-      <Part info = {props.infos[2]} />
+      <Part info = {props.parts[0]} />
+      <Part info = {props.parts[1]} />
+      <Part info = {props.parts[2]} />
     </>
   )
 }
