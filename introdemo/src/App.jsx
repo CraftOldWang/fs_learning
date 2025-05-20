786d2a33b2be022b0aba454b03ab0581ec68ccb3
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-20 19:59:26 +0800

    learning Part1.3 done

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,37 +1,48 @@
 import { useState } from 'react'
 
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
+const Display = ({counter}) => <div>{counter}</div>
 
-  const handleClick = () => {
-    console.log('clicked')
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
   }
 
-  // setTimeout(
-  //   () => setCounter(counter + 1),
-  //   1000
-  // )
-
-  console.log('rendering...', counter)
-
-  const increaseByOne = () => setCounter(counter +1 )
-  
-
-  const setToZero = () => setCounter(0)
-  
   return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
-    </div>
-
+    <>
+      <Display counter = {counter} />
+      <Button 
+        onClick = {increaseByOne} 
+        text = 'plus'
+      />
+      <Button 
+        onClick = {setToZero} 
+        text = 'zero'
+      />
+      <Button 
+        onClick = {decreaseByOne}
+        text = 'minus'
+      /> 
+    </>
   )
+
+
+
 }
 
 export default App
