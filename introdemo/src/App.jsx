5436c11fac7d64e9813a4b8a26c88ab5842f09b2
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-21 12:07:12 +0800

    learning Part1.4 done

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,48 +1,70 @@
 import { useState } from 'react'
 
-
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
-
-
-const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
   return (
-    <>
-      <Display counter = {counter} />
-      <Button 
-        onClick = {increaseByOne} 
-        text = 'plus'
-      />
-      <Button 
-        onClick = {setToZero} 
-        text = 'zero'
-      />
-      <Button 
-        onClick = {decreaseByOne}
-        text = 'minus'
-      /> 
-    </>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
-
-
-
 }
 
+const Button = ({onClick, text}) =>
+  <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+  const [value, setValue] = useState(0)
+
+  const handleLeftClick = () => {
+    setLeft(left+1)
+    const updatedLeft = left + 1;
+    setAll(allClicks.concat('L'))
+    setTotal(updatedLeft+ right)
+  }
+
+  const handleRightClick = () => {
+    setRight(right+1)
+    const updatedRight = right + 1;
+    setAll(allClicks.concat('R'))
+    setTotal(left + updatedRight);
+  }
+
+  const hello = (who) => () => {
+    console.log('hello', who)
+  }
+  
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+
+
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
+      <Button onClick={hello("craftoldw")} text = 'test' />
+
+
+      <p>value {value}</p>
+      <Button onClick={setToValue(1000)} text = 'thousand' />
+      <Button onClick={setToValue(0)} text='reset' />
+    </div>
+  )
+}
 export default App
