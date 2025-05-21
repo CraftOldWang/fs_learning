23277ff1bacd3243f3fc27e39b7fb934a0e49a8e
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-21 21:09:08 +0800

    Ex2.6 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+    const newPerson = {
+      name: newName //todo 如果用了大括号会怎样？？？？？
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const handlerNameChange = (event) => {
+    console.log(event.target.value) 
+    setNewName(event.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handlerNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map((person,id)=> <div key = {id}>{person.name}</div> )
+        // 这个有点丑陋， 不应该用数组下标作为id的
+      }
+    </div>
   )
 }
 
