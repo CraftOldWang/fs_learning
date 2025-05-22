0fd50aea899a59f7fbef316196fcedb64439ed5e
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-22 20:47:48 +0800

    Learn Part2.d done

---
+++ part2/learn_part2/src/services/notes.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting))
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default {getAll, create, update} // 这个实际是对象， 只不过是 属性:值  刚好是一个标识符时的 缩写方式。
+
+// export default { 
+//   getAll: getAll, 
+//   create: create, 
+//   update: update 
+// }
