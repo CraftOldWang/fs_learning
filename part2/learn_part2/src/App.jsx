120c93e81c2db7a33284db7d51a3f71bce46ad72
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-22 09:31:53 +0800

    Ex2.7-2.9 done

--- part2/learn_part2/src/App.jsx
+++ part2/learn_part2/src/App.jsx
@@ -1,62 +1,59 @@
-
-import Note from './components/Note'
-import { useState } from 'react'
-
+import Note from "./components/Note";
+import { useState } from "react";
 
 const App = (props) => {
-  const [notes,setNotes] = useState(props.notes)  
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(true);
 
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important);
 
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-    const newObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id:String(notes.length + 1),
-    }
-    setNotes(notes.concat(newObject))
-    setNewNote('')
-  }
+    const addNote = (event) => {
+        event.preventDefault();
+        console.log("button clicked", event.target);
+        const newObject = {
+            content: newNote,
+            important: Math.random() < 0.5,
+            id: String(notes.length + 1),
+        };
+        setNotes(notes.concat(newObject));
+        setNewNote("");
+    };
 
-  const handlerNoteChange = (event) => {
-    console.log(event.target)
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    const handlerNoteChange = (event) => {
+        console.log(event.target);
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    };
 
-  const toggleText = showAll 
-  ? "important"
-  : "all"
+    const toggleText = showAll ? "important" : "all";
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={()=>{setShowAll(!showAll)}}>
-          show {showAll? "important": "all"}
-        </button>
-      </div>
-      
-      <ul>
-        {notesToShow.map(note=> 
-          <Note note={note} key={note.id}  />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input 
-          value = {newNote}
-          onChange={handlerNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>   
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button
+                    onClick={() => {
+                        setShowAll(!showAll);
+                    }}
+                >
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
 
-export default App
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note note={note} key={note.id} />
+                ))}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handlerNoteChange} />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    );
+};
+
+export default App;
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,45 +1,120 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    // const [persons, setPersons] = useState([
+    //     { name: "Arto Hellas", number: "114514" },
+    // ]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    ]);
+    const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const [filterName, setFilterName] = useState("");
 
-  const addName = (event) => {
-    event.preventDefault()
-    console.log(event.target)
-    const newPerson = {
-      name: newName //todo 如果用了大括号会怎样？？？？？
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-  }
+    const addName = (event) => {
+        event.preventDefault();
+        console.log(event.target);
+        const newPerson = {
+            name: newName, //todo 如果用了大括号会怎样？？？？？
+        };
+        const hasEqual = persons.reduce(
+            (hasFindEq, curPerson) => hasFindEq || newName === curPerson.name,
+            false
+        );
 
-  const handlerNameChange = (event) => {
-    console.log(event.target.value) 
-    setNewName(event.target.value)
-  }
+        console.log("hasEqual", hasEqual);
 
-  return (
-    <div>
-      <div>debug: {newName}</div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+        if (hasEqual) {
+            alert(`${newName} is already added to phonebook`);
+            // setNewName("");
+        } else {
+            setPersons(persons.concat(newPerson));
+            setNewName("");
+        }
+    };
+
+    const handlerNameChange = (event) => {
+        console.log(event.target.value);
+        setNewName(event.target.value);
+    };
+
+    const handlerNumberChange = (event) => {
+        console.log(event.target.value);
+        setNewNumber(event.target.value);
+    };
+
+    const handlerFilterNameChange = (event) => {
+        console.log(event.target.value);
+        setFilterName(event.target.value);
+    };
+
+    const addContact = (event) => {
+        event.preventDefault();
+        console.log(event.target);
+        const newPerson = {
+            name: newName, //todo 如果用了大括号会怎样？？？？？
+            number: newNumber,
+            id: persons.length + 1, // 姑且这样。。。
+        };
+        const hasEqual = persons.reduce(
+            (hasFindEq, curPerson) =>
+                hasFindEq ||
+                (newName === curPerson.name && newNumber === curPerson.number),
+            false
+        );
+
+        console.log("hasEqual", hasEqual);
+
+        if (hasEqual) {
+            alert(`${newName} : ${newNumber},is already added to phonebook`);
+            // setNewName("");
+        } else {
+            setPersons(persons.concat(newPerson));
+            setNewName("");
+            setNewNumber("");
+        }
+    };
+    return (
         <div>
-          name: <input value={newName} onChange={handlerNameChange}/>
+            {/* <div>debug: {newName}</div> */}
+            <h2>Phonebook</h2>
+            <div>
+                filter shown with:{" "}
+                <input value={filterName} onChange={handlerFilterNameChange} />
+            </div>
+            <h2>Add a new</h2>
+            <form onSubmit={addContact}>
+                <div>
+                    name: <input value={newName} onChange={handlerNameChange} />
+                </div>
+                <div>
+                    number:
+                    <input value={newNumber} onChange={handlerNumberChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {
+                persons
+                    .filter((person) =>
+                        person.name
+                            .toLowerCase()
+                            .includes(filterName.toLowerCase())
+                    )
+                    .map((person) => (
+                        <div key={person.id}>
+                            {person.name} {person.number}
+                        </div>
+                    ))
+                // 这个有点丑陋， 不应该用数组下标作为id的
+            }
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {
-        persons.map((person,id)=> <div key = {id}>{person.name}</div> )
-        // 这个有点丑陋， 不应该用数组下标作为id的
-      }
-    </div>
-  )
-}
+    );
+};
 
-export default App
+export default App;
