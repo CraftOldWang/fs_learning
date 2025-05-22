c01ff8d1d03cebc0a6028b2b19318bdabb727191
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-22 16:01:26 +0800

    Ex2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
-import Filter from './components/Filter'
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
-
+import axios from "axios";
 
 const App = () => {
-    // const [persons, setPersons] = useState([
-    //     { name: "Arto Hellas", number: "114514" },
-    // ]);
-    const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "040-123456", id: 1 },
-        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [filterName, setFilterName] = useState("");
+
+    useEffect(() => {
+        console.log("effect");
+        axios.get("http://localhost:3001/persons").then(response => {
+            console.log("promise fufilled");
+            setPersons(response.data);
+        }); // 改一下，不用http  或者用https 会怎样？
+    },[]);
+
+    console.log("render", persons.length, " info")
+
+    
 
     const addName = (event) => {
         event.preventDefault();
@@ -58,9 +62,9 @@ const App = () => {
     const addContact = (event) => {
         event.preventDefault();
         console.log(event.target);
-        console.log({newName})
+        console.log({ newName });
         const newPerson = {
-            name: newName, //todo 如果用了大括号会怎样？？？？？ 
+            name: newName, //todo 如果用了大括号会怎样？？？？？
             // 会变成 对象 {newName: newName}  (key 是 newName, value 是 newName )
             //如果要访问与原来一样的那个值， 原来是 newPerson.name
             //现在是 newPerson.name.newName
