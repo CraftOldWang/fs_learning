f9ed22b0f9aa605a8a7459610f644b8041845071
CraftOldW <craftoldwmathandacg@gmail.com>
2025-05-25 22:28:06 +0800

    part2.e 还没学完，姑且跳过

--- part2/learn_part2/src/App.jsx
+++ part2/learn_part2/src/App.jsx
@@ -1,13 +1,23 @@
 import Note from "./components/Note";
+import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import noteService from "./services/notes";
 
+const Notification = ({ message }) => {
+    if (message === null) {
+        return null;
+    }
+
+    return <div className="error">{message}</div>;
+};
 const App = () => {
     const [notes, setNotes] = useState([]);
 
     const [newNote, setNewNote] = useState("");
     const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("")//useState("some error happened...");
+
     const notesToShow = showAll
         ? notes
         : notes.filter((note) => note.important);
@@ -55,16 +65,24 @@ const App = () => {
                 );
             })
             .catch((error) => {
-                alert(
-                    `the note '${note.content}' was already deleted from server`
+                // alert(
+                //     `the note '${note.content}' was already deleted from server`
+                // );
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
                 );
-                setNotes(notes.filter((n) => n.id !== id));//把那个实际不存在的note删掉
+                setTimeout(() => {
+                    setErrorMessage(null);
+                }, 5000);
+
+                setNotes(notes.filter((n) => n.id !== id)); //把那个实际不存在的note删掉
             });
     };
 
     return (
         <div>
             <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>
                 <button
                     onClick={() => {
@@ -88,6 +106,7 @@ const App = () => {
                 <input value={newNote} onChange={handlerNoteChange} />
                 <button type="submit">save</button>
             </form>
+            <Footer />
         </div>
     );
 };
